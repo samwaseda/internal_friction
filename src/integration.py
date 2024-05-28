8a530d2a09b6232b543781b6b5ae2ebc59c5b2fd
Sam Waseda <o.waseda@mpie.de>
2024-05-28 15:11:52 +0200

    add measurement

--- src/integration.py
+++ src/integration.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from scipy.spatial import cKDTree
+from pint import UnitRegistry
+from functools import cached_property, cache
+from scipy.sparse import coo_matrix
 
 
 def get_diff_E_P(mode, P_dict, structure, cutoff_radius=np.inf):
@@ -86,3 +89,187 @@ def get_mu_0(mu, c_target, kBT, N=None, max_steps=30, dmu=-0.1):
         if dc * dmu < 0:
             dmu *= -0.5
     return mu_0
+
+
+class Measurement:
+    def __init__(
+        self,
+        structure,
+        P_all_dict,
+        c_C,
+        c_v,
+        v_diff=1.0,
+        C_diff=1.2,
+        std_concentration=0.001,
+    ):
+        self._phi = None
+        self._structure = structure
+        P_C_diff_v, E_C_diff_v = get_diff_E_P(
+            "Fe", P_all_dict["C_diff_v"], structure=structure
+        )
+        P_v_diff_C, E_v_diff_C = get_diff_E_P(
+            "C", P_all_dict["v_diff_C"], structure=structure
+        )
+
+        self.E_diff_dict = {"v": E_v_diff_C, "C": E_C_diff_v}
+        self.P_diff_dict = {"v": P_v_diff_C, "C": P_C_diff_v}
+        self._dict_element = {"Fe": "Fe", "v": "Fe", "C": "C"}
+        self.concentration = {"C": c_C, "v": c_v}
+        self.diffusion_barrier = {"v": v_diff, "C": C_diff}
+        ureg = UnitRegistry()
+        self.kB = (1 * ureg.boltzmann_constant * ureg.kelvin).to("eV").magnitude
+        self.std_concentration = std_concentration
+
+    @cached_property
+    def matrix(self):
+        return self._structure[self._structure.get_chemical_symbols() != "O"]
+
+    @property
+    def phi(self):
+        if self._phi is None:
+            phi_C = 1 + self.std_concentration * np.random.randn(
+                len(self.get_index("C"))
+            )
+            phi_C = (
+                self.concentration["C"]
+                / (1 - self.concentration["C"])
+                * phi_C
+                / phi_C.mean()
+            )
+            phi_v = 1 + self.std_concentration * np.random.randn(
+                len(self.get_index("v"))
+            )
+            phi_v = (
+                self.concentration["v"]
+                / (1 - self.concentration["v"])
+                * phi_v
+                / phi_v.mean()
+            )
+            self._phi = np.zeros(len(self.matrix))
+            self._phi[self.get_index("v")] = phi_v
+            self._phi[self.get_index("C")] = phi_C
+        return self._phi
+
+    @cache
+    def get_index(self, element):
+        return self.matrix.select_index(self._dict_element[element])
+
+    def get_phi(self, element):
+        return self.phi[self.get_index(element)]
+
+    def set_phi(self, new_phi, element=None):
+        if element is not None:
+            self._phi[self.get_index(element)] = new_phi
+        else:
+            self._phi = new_phi
+
+    @cached_property
+    def _mat(self):
+        return get_E_P(structure=self.matrix)
+
+    @property
+    def E_mat(self):
+        return self._mat[1]
+
+    @property
+    def P_mat(self):
+        return self._mat[0]
+
+    def get_diff(self, elem_1, elem_2):
+        return (
+            self.E_diff_dict[elem_1] @ self.phi[self.get_index(elem_2)]
+            + self.diffusion_barrier[elem_1]
+        )
+
+    @property
+    def narrow_path(self):
+        return self._structure.positions[self._structure.select_index("O")]
+
+    @cached_property
+    def pairs(self):
+        return {
+            key: self.matrix[self.get_index(key)]
+            .get_neighborhood(self.narrow_path, num_neighbors=2)
+            .indices
+            for key in ["C", "v"]
+        }
+
+    def get_E_sub(self, epsilon=None):
+        E = {
+            key: (self.E_mat @ self.phi)[self.get_index(key)][self.pairs[key]]
+            for key in ["C", "v"]
+        }
+        if epsilon is not None:
+            P = np.einsum("nmij,m->nij", self.P_mat, self.phi)
+            P_sub = {key: P[self.get_index(key)] for key in ["C", "v"]}
+            E = {
+                key: value
+                + np.einsum("nij,ij->n", P_sub[key], epsilon)[self.pairs[key]]
+                for key, value in E.items()
+            }
+        return E
+
+    def get_E_diff(self, epsilon=None):
+        E_diff = {"C": self.get_diff("C", "v"), "v": self.get_diff("v", "C")}
+        if epsilon is not None:
+            P = self.P_diff_dict
+            E_diff = {
+                key: value
+                + np.einsum(
+                    "nmij,m,ij->n", P[key], self.phi[self.get_index(key)], epsilon
+                )
+                for key, value in E_diff.items()
+            }
+        E_sub = self.get_E_sub(epsilon=epsilon)
+        return {key: E_diff[key][:, None] - E_sub[key] for key in ["C", "v"]}
+
+    @cached_property
+    def inv_volume(self):
+        volume = self._structure.get_volume()
+        ureg = UnitRegistry()
+        return (1 / volume * ureg.electron_volt / ureg.angstrom**3).to(
+            "Pa"
+        ).magnitude / 1e9
+
+    def get_pressure(self):
+        return (
+            0.5
+            * np.einsum("n,nmij,m->ij", self.phi, self.P_mat, self.phi)
+            * self.inv_volume
+        )
+
+    def get_K(self, temperature, epsilon=None, kappa=1.0e13):
+        kBT = self.kB * temperature
+        K = {}
+        E = self.get_E_diff(epsilon=epsilon)
+        for key, pair in self.pairs.items():
+            pairs = np.concatenate((pair, pair[:, ::-1]), axis=0)
+            K[key] = coo_matrix(
+                (np.exp(np.log(kappa) - E[key].T.flatten() / kBT), tuple(pairs.T))
+            )
+        return K
+
+    def get_chemical_potential(self, temperature, element=None, normalize=True):
+        kBT = self.kB * temperature
+        mu = self.E_mat @ self.phi + kBT * np.log(self.phi / (1 - self.phi))
+        if normalize:
+            for key in ["C", "v"]:
+                mu[self.get_index(key)] -= get_mu_0(
+                    mu[self.get_index(key)], self.concentration[key], kBT
+                )
+        if element is None:
+            return mu
+        else:
+            return mu[self.get_index(element)]
+
+    def get_internal_energy(self):
+        return 0.5 * self.phi @ self.E_mat @ self.phi
+
+    def get_entropy(self):
+        return (
+            -self.kB
+            * (self.phi * np.log(self.phi) + (1 - self.phi) * (1 - self.phi)).sum()
+        )
+
+    def get_free_energy(self, temperature):
+        return self.get_internal_energy() + temperature * self.get_entropy()
